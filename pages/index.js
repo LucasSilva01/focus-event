772e86ed7b801632a00dd234d811a3d0ed3076b1
LucasSilva01 <santoslucas9956@gmail.com>
2021-03-09 19:22:52 -0300

    updated navigation bar

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,26 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className = {styles.navbar}>
-      <a href = "#">Home</a>
-      <a href = "#">Eventos</a>
-      <a href = "#">Produtores</a>
-      <a href = "#">Ajude-me</a>
-        
-      <div className = {styles.button}>
-        <a href = "#">Login</a>
-      </div>
+    <div>
+      <div className = {styles.navbar}>
+        <a href = "#">Home</a>
+        <a href = "#">Eventos</a>
+        <a href = "#">Produtores</a>
+        <a href = "#">Ajude-me</a>
+          
+        <div className = {styles.button}>
+          <a href = "#">Login</a>
+        </div>
 
-      <div className = {styles.login}>
-        <a href = "#">Cadastre-se</a>
+        <div className = {styles.login}>
+          <a href = "#">Cadastre-se</a>
+        </div>
+      </div>
+      
+      <div className = {styles.groupElements}>
+        <div className = {styles.organize}>
+          <a href = "#">Organizar evento</a>
+        </div>
       </div>
     </div>
   )
