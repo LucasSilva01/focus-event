abe6c8674f316b76423e2b186df69b4256d86fc5
LucasSilva01 <santoslucas9956@gmail.com>
2021-03-09 16:57:37 -0300

    navigation bar added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Projeto de Pweb</h1>
+    <div className = {styles.navbar}>
+      <a href = "#">Home</a>
+      <a href = "#">Eventos</a>
+      <a href = "#">Produtores</a>
+      <a href = "#">Ajude-me</a>
+        
+      <div className = {styles.button}>
+        <span class = "button"><a href = "#">Login</a></span>
+      </div>
+      
+      <div className = {styles.login}>
+        <a href = "#">Cadastre-se</a>
+      </div>
     </div>
   )
 }
