9ca86c1e2bc6b822f883e03af539d653d8a3c81a
LucasSilva01 <santoslucas9956@gmail.com>
2021-03-18 15:01:25 -0300

    visual changes

--- components/map.js
+++ components/map.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps";
 
 
-const loadingElementStyle = { height: '100%', width: '100%' };
-const containerElementStyle = { height: '555px'};
-const mapElementStyle = { height: '100%' , width: '100%'};
+const loadingElementStyle = { height: '100%'};
+const containerElementStyle = { height: '440px', wigth: '300px'};
+const mapElementStyle = { height: '100%'};
 const defaultCenter = { lat: -6.459966, lng: -37.093687 };
 const defaultOptions = { scrollwheel: false };
+import styles from '../styles/Home.module.css'
 
 const RegularMap = withScriptjs(
   withGoogleMap(props => (
@@ -23,13 +24,14 @@ const RegularMap = withScriptjs(
 
 function GoogleMaps(){
   return (
-
+    <div className = {styles.groupElements} >
     <RegularMap
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyANvb7FpQ74Tnwgyj460zTZPOd4NWeeiTc"
       loadingElement={<div style={ loadingElementStyle } />}
       containerElement={<div style={ containerElementStyle}/>}
       mapElement={<div style={ mapElementStyle } />}
     />
+    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ import Mapa from '../components/map'
 
 export default function Home() {
   return (
-    <div>
+    <div className = {styles.backG} >
 
-        <div className = {styles.navbar}>
-          
+        <div >
+          <div className = {styles.navbar}>
           <a href = "/"><Image src="/../public/house-png-190.png"
             width={40}
             height={40}
@@ -20,6 +20,7 @@ export default function Home() {
           <a href = "#">Produtores</a>
           <a href = "#">Ajude-me</a>
           
+          
           <div  className = {styles.button}>
             <a href = "/login"><Image src="/../public/loginButtou.png"
             width={40}
@@ -27,16 +28,34 @@ export default function Home() {
           
           /></a>
           </div>
+          
+         
 
           <div className = {styles.login}>
             <a href = "/register">Cadastre-se</a>
           </div>
-        </div>
         
-        <div>
-          <Mapa />
+
+          
         </div>
- 
+        </div>
+        <div className = {styles.configLogo}>
+            <a >
+            <Image src="/../public/focus.png"
+            
+            width={270}
+            height={100}
+            
+          />
+            </a>
+          </div>
+        
+          <div>
+          <Mapa />
+          </div>
+
+
+
           
       <div className = {styles.footer}>
 
