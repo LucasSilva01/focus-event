290e7905af1eea63a395f13017b7abc87eaed6e4
LucasSilva01 <santoslucas9956@gmail.com>
2021-03-22 22:40:04 -0300

    organizing folders

---
+++ components/footer/footer.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import styles from '../css/Home.module.css'
+
+
+function Footer(){
+    return(
+        <div className = {styles.footer}>
+          <p>O Focus-Event é um projeto acadêmico avaliativo da disciplina de Programação Web, do curso de Sistemas de Informação.&nbsp;&nbsp;&nbsp;</p>
+            <a href="https://github.com/LucasSilva01/focus-event">
+              <Image
+                src="/../public/logogithub.png"
+                alt="GitHub do Projeto"
+                width={76}
+                height={76}
+              
+              /></a>
+          <a className = {styles.card} href = "#">Organizar evento </a>
+        </div>
+    )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import login from './login'
 import Mapa from '../components/map'
 import Header from '../components/header/header'
 import Navbar from '../components/navbar/navbar'
+import Footer from '../components/footer/footer'
 
 export default function Home() {
   return (
@@ -21,19 +22,9 @@ export default function Home() {
             <Mapa />
             </div>
   
-      <div className = {styles.footer}>
-          <p>O Focus-Event é um projeto acadêmico avaliativo da disciplina de Programação Web, do curso de Sistemas de Informação.&nbsp;&nbsp;&nbsp;</p>
-            <a href="https://github.com/LucasSilva01/focus-event">
-              <Image
-                src="/../public/logogithub.png"
-                alt="GitHub do Projeto"
-                width={76}
-                height={76}
-              
-              /></a>
-          <a className = {styles.card} href = "#">Organizar evento </a>
-        </div>
+      
       </div>
+      <Footer/>
     </div>
   )
 }
